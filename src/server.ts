5cd61009474089816ff61d2f9324ac586d24d5eb
Gabriel Martins Rennó <gabrielmrenno@gmail.com>
2022-03-22 19:12:08 -0300

    Tratamento de exceções com express-async-errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 
 import { routes } from "./routes";
 
@@ -7,5 +9,18 @@ const app = express();
 app.use(express.json());
 
 app.listen(3333, () => console.log("Server is running"));
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            message: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: "internal Server Error"
+    })
+})
 
 app.use(routes);
